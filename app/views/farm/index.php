29dc954686745029295fbbd774264d8ca4994e9b
adityapandey89 <aditya.pandey@infinitylabs.in>
2018-12-13 15:08:34 +0530

    Removed Unused variables and optimizing as per coding STD

--- app/views/farm/index.php
+++ app/views/farm/index.php
@@ -4,20 +4,22 @@
   Author     : groot (Aditya Pandey)
   Description: The main view file to display the FARM GAME UI
 -->
-
+<?php
+$uri = $_SERVER['REQUEST_URI'];
+?>
 <html>
     <head>
         <title>Farm Game</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="<?= $_SERVER['REQUEST_URI'] ?>/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
-        <link href="<?= $_SERVER['REQUEST_URI'] ?>/css/common.css" rel="stylesheet" type="text/css"/>
-        <script src="<?= $_SERVER['REQUEST_URI'] ?>/scripts/jquery.min.js" type="text/javascript"></script>
-        <script src="<?= $_SERVER['REQUEST_URI'] ?>/scripts/bootstrap.min.js" type="text/javascript"></script>
-        <script src="<?= $_SERVER['REQUEST_URI'] ?>/scripts/custom.js" type="text/javascript"></script>
+        <link href="<?= $uri ?>/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <link href="<?= $uri ?>/css/common.css" rel="stylesheet" type="text/css"/>
+        <script src="<?= $uri ?>/scripts/jquery.min.js" type="text/javascript"></script>
+        <script src="<?= $uri ?>/scripts/bootstrap.min.js" type="text/javascript"></script>
+        <script src="<?= $uri ?>/scripts/custom.js" type="text/javascript"></script>
     </head>
     <body>
-        <input type="hidden" class="request_uri" value="<?= $_SERVER['REQUEST_URI'] ?>"/>
+        <input type="hidden" class="request_uri" value="<?= $uri ?>"/>
         <nav class="navbar navbar-default">
             <div class="container-fluid">
                 <div class="navbar-header">
@@ -43,21 +45,21 @@
             <table class="table table-bordered feed_table">
                 <thead>
                     <tr>
-                        <th><img src="<?= $_SERVER['REQUEST_URI'] ?>/images/time.png" alt="Round"/></th>
-                        <th style="<?= (in_array('FARMER', $data['life'])) ? 'background-color:#F05F5F' : ""; ?>"><img src="<?= $_SERVER['REQUEST_URI'] ?>/images/farmer.png" alt="Famer"/></th>
-                        <th style="<?= (in_array('COW_1', $data['life'])) ? 'background-color:#F05F5F' : ""; ?>"><img src="<?= $_SERVER['REQUEST_URI'] ?>/images/cow.png" alt="Cow 1"/>1</th>
-                        <th style="<?= (in_array('COW_2', $data['life'])) ? 'background-color:#F05F5F' : ""; ?>"><img src="<?= $_SERVER['REQUEST_URI'] ?>/images/cow.png" alt="Cow 2"/>2</th>
-                        <th style="<?= (in_array('BUNNY_1', $data['life'])) ? 'background-color:#F05F5F' : ""; ?>"><img src="<?= $_SERVER['REQUEST_URI'] ?>/images/bunny.png" alt="Bunny 1"/>1</th>
-                        <th style="<?= (in_array('BUNNY_2', $data['life'])) ? 'background-color:#F05F5F' : ""; ?>"><img src="<?= $_SERVER['REQUEST_URI'] ?>/images/bunny.png" alt="Bunny 2"/>2</th>
-                        <th style="<?= (in_array('BUNNY_3', $data['life'])) ? 'background-color:#F05F5F' : ""; ?>"><img src="<?= $_SERVER['REQUEST_URI'] ?>/images/bunny.png" alt="Bunny 3"/>3</th>
-                        <th style="<?= (in_array('BUNNY_4', $data['life'])) ? 'background-color:#F05F5F' : ""; ?>"><img src="<?= $_SERVER['REQUEST_URI'] ?>/images/bunny.png" alt="Bunny 4"/>4</th>
+                        <th><img src="<?= $uri ?>/images/time.png" alt="Round"/></th>
+                        <th style="<?= (in_array('FARMER', $data['life'])) ? 'background-color:#F05F5F' : ""; ?>"><img src="<?= $uri ?>/images/farmer.png" alt="Famer"/></th>
+                        <th style="<?= (in_array('COW_1', $data['life'])) ? 'background-color:#F05F5F' : ""; ?>"><img src="<?= $uri ?>/images/cow.png" alt="Cow 1"/>1</th>
+                        <th style="<?= (in_array('COW_2', $data['life'])) ? 'background-color:#F05F5F' : ""; ?>"><img src="<?= $uri ?>/images/cow.png" alt="Cow 2"/>2</th>
+                        <th style="<?= (in_array('BUNNY_1', $data['life'])) ? 'background-color:#F05F5F' : ""; ?>"><img src="<?= $uri ?>/images/bunny.png" alt="Bunny 1"/>1</th>
+                        <th style="<?= (in_array('BUNNY_2', $data['life'])) ? 'background-color:#F05F5F' : ""; ?>"><img src="<?= $uri ?>/images/bunny.png" alt="Bunny 2"/>2</th>
+                        <th style="<?= (in_array('BUNNY_3', $data['life'])) ? 'background-color:#F05F5F' : ""; ?>"><img src="<?= $uri ?>/images/bunny.png" alt="Bunny 3"/>3</th>
+                        <th style="<?= (in_array('BUNNY_4', $data['life'])) ? 'background-color:#F05F5F' : ""; ?>"><img src="<?= $uri ?>/images/bunny.png" alt="Bunny 4"/>4</th>
                     </tr>
                 </thead>
                 <tbody>
                     <?php
                     if (!empty($data['record'])) {
                         foreach ($data['record'] as $round => $fedLife) {
-                            $image = '<img style="height:50px;width:50px;" src="' . $_SERVER['REQUEST_URI'] . '/images/food.png" alt="Fed"/>';
+                            $image = '<img style="height:50px;width:50px;" src="' . $uri . '/images/food.png" alt="Fed"/>';
                             ?>
                             <tr>
                                 <td><?= $round + 1 ?></td>
